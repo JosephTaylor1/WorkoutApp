b04f5e97e0a69789fbadab22039d0a7c493c8e12
JosephTaylor1 <joetaylor2252@gmail.com>
2023-01-19 15:55:08 -0500

    initial commit for login form

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import {
+    Form
+} from './HomepageFormElements';
 
 const HomePage = (props) => {
     return(
         <div>
-            <h1>HEEEELLLLOOOOOOOO</h1>
+            <Form
+                  component="form"
+                  sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                  }}
+                  noValidate
+                  autoComplete="off"
+            >
+                <TextField id="outlined-basic" label="Username:" variant="outlined" />
+                <TextField id="outlined-basic" label="Password:" variant="outlined" />
+                <Button variant="contained">Submit</Button>
+            </Form>
+
         </div>
     );
 };
