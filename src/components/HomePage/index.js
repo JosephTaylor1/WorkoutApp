9f0eaff3e27626fbb4e537c8f137990724c0526e
JosephTaylor1 <joetaylor2252@gmail.com>
2023-01-24 18:36:22 -0500

    login/logout backend start

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import {
     Form
 } from './HomepageFormElements';
+import Stack from '@mui/material/Stack';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../firebase-config';
 
 const HomePage = (props) => {
+
+    const [registerEmail, setRegisterEmail] = useState('');
+    const [registerPassword, setRegisterPassword] = useState('');
+
+    
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+
+    const [ user, setUser ] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    });
+
+    const register = async () => {
+        try {
+          const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+          console.log(user)
+        } catch (error) {
+            console.log(error.message)
+        }
+    };
+
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+            console.log(user)
+          } catch (error) {
+              console.log(error.message)
+          }
+    };
+
+    const logout = async () =>{
+        await signOut(auth);
+    };
+
     return(
         <div>
             <Form
@@ -16,9 +55,25 @@ const HomePage = (props) => {
                   noValidate
                   autoComplete="off"
             >
-                <TextField id="outlined-basic" label="Username:" variant="outlined" />
-                <TextField id="outlined-basic" label="Password:" variant="outlined" />
-                <Button variant="contained">Submit</Button>
+                <Stack>
+                    <TextField onChange={(e) => { setRegisterEmail(e.target.value)}} id="outlined-basic" label="Email:" variant="outlined" />
+                    <TextField onChange={(e) => { setRegisterPassword(e.target.value)}} id="outlined-basic" label="Password:" variant="outlined" />
+                    <Button onClick={register} variant="contained">Create User</Button>
+                </Stack>
+
+                <Stack>
+                    <TextField onChange={(e) => { setLoginEmail(e.target.value)}} id="outlined-basic" label="Email:" variant="outlined" />
+                    <TextField onChange={(e) => { setLoginPassword(e.target.value)}} id="outlined-basic" label="Password:" variant="outlined" />
+                    <Button onClick={login} variant="contained">Login</Button>
+                </Stack>
+
+                <h4>Logged In As:</h4>
+                {user?.email}
+
+    
+                <Stack>
+                    <Button onClick={logout} variant="contained">Sign Out</Button>
+                </Stack>
             </Form>
 
         </div>
